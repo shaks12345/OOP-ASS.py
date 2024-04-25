8de350c21f1208e29e0c816de806a14d715c9ced
shaks12345 <159923949+shaks12345@users.noreply.github.com>
2024-04-25 09:16:36 +0300

    Add files via upload

---
+++ person.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and I am {self.gender}.")
+
+# Creating an instance of the Person class
+example_person = Person("Gloria", 21, "female")
+
+# Calling the introduce method to display the person's information
+example_person.introduce()
